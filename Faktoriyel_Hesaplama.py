f7c20a109048158a9d0b86f9f85c549f2da9dbe5
Hakan SARIER <sarierhakan@gmail.com>
2022-08-31 11:59:06 +0300

    refactor1

--- Faktoriyel_Hesaplama.py
+++ Faktoriyel_Hesaplama.py
@@ -2,15 +2,11 @@
 
 sayi = input("Faktoriyelini hesaplamak istediginiz sayiyi giriniz: ")
 faktoriyel = 1
-
 while not sayi.isdigit():
     print("Gecersiz deger girdiniz.")
     sayi = input("Lutfen faktoriyelini hesaplamak istediginiz sayiyi giriniz: ")
-else:
-    if int(sayi) == 0:
-        print("0! = 1")
-    else:
-        for i in range(1,int(sayi)+1):
-            faktoriyel*=i   
-        print("{0}! = {1}".format(sayi,faktoriyel))
 
+for i in range(1, int(sayi) + 1):
+    faktoriyel *= i
+
+print("{0}! = {1}".format(sayi, faktoriyel))
